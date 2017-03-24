d66c5894a7710161507dff6535567823a69ea08b
Yaroslav Bulatov <yaroslavvb@gmail.com>
2017-03-23 20:30:13 -0700

    Fix step-length for rank-deficient data
    This makes it converge in one step on rank-deficient data

--- exp/linear-rankdeficient.py
+++ exp/linear-rankdeficient.py
@@ -5,6 +5,8 @@
 # Accompanying notebook:
 # linear-rankdeficient-join.nb
 # https://www.wolframcloud.com/objects/3ebc256e-dc25-4513-971e-082d28fae38a
+# Fixed data (centered X's + losses) generated from
+# linear-cholesky-rankdeficient.nb
 #
 # Additional sanity checks in:
 # linear-rankdeficient.nb
@@ -67,7 +69,7 @@ def train_pseudoinverse():
   tf.reset_default_graph()
 
   dtype = tf.float64
-  XY0 = np.genfromtxt('linear-rankdeficient.csv', delimiter= ",")
+  XY0 = np.genfromtxt('linear-rankdeficient-fixed.csv', delimiter= ",")
   X0 = XY0[:-1,:]  # 3 x d
   Y0 = XY0[-1:,:]  # 1 x d
   dsize = X0.shape[1]
@@ -87,7 +89,7 @@ def train_pseudoinverse():
   covI = tf.Variable(tf.matrix_inverse(cov_), trainable=False)
 
   init_dict = {X_: X0, Y_: Y0}
-  lr = tf.constant(.15, dtype=dtype)
+  lr = tf.constant(.5, dtype=dtype)
   opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
   
   (grad,var)  = opt.compute_gradients(loss, tf.trainable_variables())[0]
@@ -97,13 +99,13 @@ def train_pseudoinverse():
   s, u, v = tf.svd(cov)
   eps = 1e-10   # threshold used to determine which singular values are zero
   si = tf.where(tf.less(s, eps), s, 1./s)
-  pre = tf.matmul(tf.matmul(u, tf.diag(si)), tf.transpose(v))
+  pre = u @ tf.diag(si) @ tf.transpose(v)
 
-  grad = tf.matmul(grad, pre)
+  grad = grad @ pre
   train_op = opt.apply_gradients([(grad, var)])
 
   sess.run(tf.global_variables_initializer(), feed_dict=init_dict)
-
+  
   observed_losses = []
   for i in range(100):
     loss0, _ = sess.run([loss, train_op])
@@ -112,11 +114,11 @@ def train_pseudoinverse():
   # from accompanying notebook
   # [1.04094, 0.510063, 0.249931, 0.122466, 0.0600084, 0.0294041,
   #  0.014408, 0.00705992, 0.00345936, 0.00169509, 0.000830593...
-  expected_losses = np.loadtxt("linear-rankdeficient-losses-pre.csv")
+  expected_losses = np.loadtxt("linear-rankdeficient-losses-pre-fixed.csv")
   np.testing.assert_allclose(expected_losses[:10],
-                             observed_losses[:10], rtol=1e-12)
+                            observed_losses[:10], rtol=1e-10, atol=1e-20)
 
 
 if __name__ == '__main__':
-  train_regular()
+  #train_regular()
   train_pseudoinverse()
