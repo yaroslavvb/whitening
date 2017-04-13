8f0870f4fbda604298901f8f26d4b07f06801e5c
Yaroslav Bulatov <yaroslavvb@gmail.com>
2017-04-12 20:37:22 -0700

    more refactoring

--- mnist_autoencoder/train2.py
+++ mnist_autoencoder/train2.py
@@ -26,25 +26,11 @@ from util import v2r
 from util import kr  # kronecker
 from util import Kmat # commutation matrix
 
-def transposeImageCols(mat):
-  """Treats columns of mat as flattened square images, transposes them"""
-  result = np.zeros(mat.shape, dtype=mat.dtype)
-  w2=mat.shape[0]
-  w = int(math.sqrt(w2))
-  assert w**2==w2
-  for i in range(mat.shape[1]):
-    patch=mat[:,i].reshape((w,w))
-    result[:,i]=patch.transpose().flatten()
-  return result
-
-def W_init(hidden_size, visible_size):
-  np.random.seed(0)
-
-  r = np.sqrt(6) / np.sqrt(hidden_size + visible_size + 1)
-  W1 = np.random.random((hidden_size, visible_size)) * 2 * r - r
-  W2 = np.random.random((visible_size, hidden_size)) * 2 * r - r
-  return np.concatenate((W1.reshape(hidden_size * visible_size),
-                         W2.reshape(hidden_size * visible_size)))
+def W_uniform(s1, s2):
+  r = np.sqrt(6) / np.sqrt(s1 + s2 + 1)
+  W1 = np.random.random((s1, s2)) * 2 * r - r
+  W2 = np.random.random((s2, s1)) * 2 * r - r
+  return np.concatenate([W1.flatten(), W2.flatten()])
 
 
 def cost_and_grad(W0f=None, fs=None, lambda_=3e-3, rho=0.1, beta=3,
@@ -66,16 +52,10 @@ def cost_and_grad(W0f=None, fs=None, lambda_=3e-3, rho=0.1, beta=3,
   tf.set_random_seed(0)
   dtype = np.float32
 
-  train_images = load_MNIST.load_MNIST_images('data/train-images-idx3-ubyte')
-  dsize = 10000
-  patches = train_images[:,:dsize];
-  X0 = transposeImageCols(patches) # todo: get rid of transpose
-  #  X0 = patches
-
   if not fs:
     fs = [dsize, 28*28, 196, 28*28]
   if not W0f:
-    W0f = W_init(fs[2],fs[3])
+    W0f = W_uniform(fs[2],fs[3])
   rho = tf.constant(rho, dtype=dtype)
 
   def f(i): return fs[i+1]  # W[i] has shape f[i] x f[i-1]
@@ -109,8 +89,8 @@ def cost_and_grad(W0f=None, fs=None, lambda_=3e-3, rho=0.1, beta=3,
   A = [None]*(n+2)
   A[0] = u.Identity(dsize, dtype=dtype)
   A[1] = W[0]
-  for i in range(1, n+1): # TODO: prettify
-    A[i+1] = sigmoid(tf.matmul(W[i], A[i], name="A"+str(i+1)))
+  for i in range(1, n+1):
+    A[i+1] = sigmoid(W[i] @ A[i])
     
 
   # reconstruction error and sparsity error
@@ -146,20 +126,28 @@ def cost_and_grad(W0f=None, fs=None, lambda_=3e-3, rho=0.1, beta=3,
   sess.run(tf.global_variables_initializer(), feed_dict=init_dict)
   return cost, train_op
   
-#  min: 9.67, median: 11.55
 def complex_train_test():
   
-  do_images = False
-  cost, train_op = cost_and_grad()
-  sess = tf.get_default_session()
+  np.random.seed(0)
   
+  do_images = True
+
+  train_images = load_MNIST.load_MNIST_images('data/train-images-idx3-ubyte')
+  dsize = 10000
+  patches = train_images[:,:dsize];
+  fs = [dsize, 28*28, 196, 28*28]
+  cost, train_op = cost_and_grad(fs=fs, X0=patches, lambda_=3e-3, rho=0.1, beta=3, lr=0.1)
+  
+  sess = tf.get_default_session()
+
+
   u.reset_time()
   old_cost = sess.run(cost)
   old_i = 0
   frame_count = 0
-  for i in range(10):
+  for i in range(1000):
     cost0, _ = sess.run([cost, train_op])
-    if i%1 == 0:
+    if i%100 == 0:
       print(cost0)
       # filters are transposed in visualization
     if ((old_cost - cost0)/old_cost > 0.05 or i-old_i>50) and do_images:
@@ -172,10 +160,6 @@ def complex_train_test():
     u.record_time()
 
   u.summarize_time()
-
-  #  cost0, _ = profiled_run([cost, train_op])
-  print(cost0, (cost0-74.17)*1000)
-  assert abs((cost0-74.17)*1000) < 20  # approximately 17.17
 
   
 if __name__=='__main__':
